9e41167e81b0aae10b48dd7445b427a2ef5075d7
Sunjot Singh <sunjot@uoguelph.ca>
2017-08-04 16:33:00 -0400

    fixed redirection bug on login/home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,24 +1,24 @@
 class WelcomeController < ApplicationController
 
-  before_filter :authenticate_user!
-
   def index
-    @ids = current_user.show_ids
 
-    # randomly pick a show from users collection to use for suggesting other shows
-    unless @ids.length <= 1
-      val = @ids.sample
+    unless current_user.nil?
+      @ids = current_user.show_ids
 
-      val = @ids.sample
-      check = Show.select("shid, poster").find(val)
-      while (check.shid.nil?)
+      # randomly pick a show from users collection to use for suggesting other shows
+      unless @ids.length <= 1
+        val = @ids.sample
+
         val = @ids.sample
         check = Show.select("shid, poster").find(val)
+        while (check.shid.nil?)
+          val = @ids.sample
+          check = Show.select("shid, poster").find(val)
+        end
+
+        @selectedShow = check;
       end
-
-      @selectedShow = check;
     end
-
   end
 
 end
